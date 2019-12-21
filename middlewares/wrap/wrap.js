91d7c7dee9a99078952d13d983c144abd5186e91
Oskar <oskar@couponfollow.com>
2019-12-21 19:58:31 +0100

    Add async middleware wrapper with error handling

--- middlewares/wrap/wrap.js
+++ middlewares/wrap/wrap.js
@@ -1,11 +1,5 @@
-/*
-  Async middleware wrapper
-  https://medium.com/@Abazhenov/using-async-await-in-express-with-node-8-b8af872c0016
-*/
-const wrap = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+const wrap = middleware => (req, res, next) => {
+    Promise.resolve(middleware(req, res, next)).catch(next);
 };
 
-module.exports = {
-    wrap
-};
+module.exports = wrap;
