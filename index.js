3885d2ea2c0d2a3025a478aaf6e66663304245d5
Oskar <oskar@couponfollow.com>
2019-12-21 14:22:43 +0100

    Initial app setup

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', (req, res) => res.send('TEST'));
+
+app.use((req, res, next) => {
+    next(new Error('Not found specific route.'));
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message || 'Unexpected error occurred, please try again later.');
+});
+
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}.`));
