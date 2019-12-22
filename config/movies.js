b64f63da3bf8e4f73e9c58bd905ccfba5eb4fe83
osanow <osanow@wp.pl>
2019-12-22 10:31:26 +0100

    Add movies config

---
+++ config/movies.js
@@ -0,0 +1,3 @@
+module.exports = {
+    requiredMovieParams: ['title']
+};
--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const { bodyValidator } = require('../../tools/bodyValidator');
+const { requireBodyParams } = require('../../tools/requireBodyParams');
 const { wrap } = require('../../middlewares/wrap');
+const moviesConfig = require('../../config/movies');
 const { movieSchema } = require('./schemas');
 
 const router = express.Router();
 
 router.post(
     '/',
+    requireBodyParams(moviesConfig.requiredMovieParams),
     bodyValidator(movieSchema),
     wrap(async (req, res) => {
         res.status(200).json({});
