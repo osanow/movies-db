f58d8ca64264d7f6ffd70e3af5aaf49b9f801572
osanow <osanow@wp.pl>
2019-12-23 19:10:43 +0100

    Add timestamps to movie model

--- models/movie.js
+++ models/movie.js
@@ -2,36 +2,39 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const schema = new Schema({
-    title: { type: String, index: true, unique: true },
-    imdbid: { type: String, index: true, unique: true },
-    year: { type: String },
-    rated: { type: String },
-    released: { type: String },
-    runtime: { type: String },
-    genre: { type: String },
-    director: { type: String },
-    writer: { type: String },
-    actors: { type: String },
-    plot: { type: String },
-    language: { type: String },
-    country: { type: String },
-    awards: { type: String },
-    metascore: { type: String },
-    imdbrating: { type: String },
-    imdbvotes: { type: String },
-    type: { type: String },
-    dvd: { type: String },
-    boxoffice: { type: String },
-    production: { type: String },
-    website: { type: String },
-    poster: { type: String },
-    ratings: [
-        {
-            Source: { type: String },
-            Value: { type: String }
-        }
-    ]
-});
+const schema = new Schema(
+    {
+        title: { type: String, index: true, unique: true },
+        imdbid: { type: String, index: true, unique: true },
+        year: { type: String },
+        rated: { type: String },
+        released: { type: String },
+        runtime: { type: String },
+        genre: { type: String },
+        director: { type: String },
+        writer: { type: String },
+        actors: { type: String },
+        plot: { type: String },
+        language: { type: String },
+        country: { type: String },
+        awards: { type: String },
+        metascore: { type: String },
+        imdbrating: { type: String },
+        imdbvotes: { type: String },
+        type: { type: String },
+        dvd: { type: String },
+        boxoffice: { type: String },
+        production: { type: String },
+        website: { type: String },
+        poster: { type: String },
+        ratings: [
+            {
+                Source: { type: String },
+                Value: { type: String }
+            }
+        ]
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.models.Movie || mongoose.model('Movie', schema);
