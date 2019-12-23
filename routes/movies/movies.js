0f30b59ebf7019201ede0444df6f82bebf22cd9d
osanow <osanow@wp.pl>
2019-12-23 16:40:55 +0100

    Use movie module for saving movies

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -4,6 +4,7 @@ const { bodyValidator } = require('../../tools/bodyValidator');
 const { requireOneOfBodyParams } = require('../../tools/requireOneOfBodyParams');
 const { wrap } = require('../../middlewares/wrap');
 const moviesConfig = require('../../config/movies');
+const movies = require('../../modules/movies');
 const { movieSchema } = require('./schemas');
 
 const router = express.Router();
@@ -13,7 +14,9 @@ router.post(
     requireOneOfBodyParams(moviesConfig.requiredMovieParams),
     bodyValidator(movieSchema),
     wrap(async (req, res) => {
-        res.status(200).json({});
+        const createdMovie = await movies.create(req.body);
+
+        res.status(200).json({ success: true, movie: createdMovie });
     })
 );
 
